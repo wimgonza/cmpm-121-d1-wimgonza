02484c1ebff27eaa8196490256126a66d7e447ea
wimgonza <167358036+wimgonza@users.noreply.github.com>
2025-10-09 10:03:18 +0000

    Added button click logic and incremental counter

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Counter: <span id="counter">0</span></p>
+  <button id="increment">Click Me!</button>
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  // console.log("I have these thingies:", button, counterElement, counter);
+  // counter incremented by 1
+  counter++;
+  // update counter text
+  counterElement.textContent = counter.toString();
+});
