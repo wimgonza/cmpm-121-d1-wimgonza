31ade7be34a3b93a1f7ff43a9e7191dabc1e29a4
wimgonza <167358036+wimgonza@users.noreply.github.com>
2025-12-05 13:26:21 +0000

    Added a button you can click (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,15 @@ let counter: number = 0;
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
   <p>Counter: <span id="counter">0</span></p>
-  <button id="increment">Click Me!</button>
 `;
 
 // Add click handler
-const button = document.getElementById("increment")!;
+const button = document.createElement("button");
+button.id = "increment";
+button.textContent = "💀";
+document.body.appendChild(button);
+
+// Counter element
 const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
